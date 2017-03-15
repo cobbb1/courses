5cdc5e2d70943ec8beb617711d5983235c3a66f4
cobbb1 <531138675@qq.com>
2017-03-15 23:22:20 +0800

    1

--- courses/mathematics/models.py
+++ courses/mathematics/models.py
@@ -17,7 +17,7 @@ class Neuron(models.Model):
 
 
 class Question(models.Model):
-    id = models.CharField(max_length=100,primary_key=True)
+    id = models.AutoField(primary_key=True)
     code = models.CharField(max_length=100)
     categorychoice = (
         (1,"Expl"),
