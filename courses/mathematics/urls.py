3bbba03af31d249ee477e8c27655f3d3f5180c72
cobbb1 <531138675@qq.com>
2017-03-16 00:01:06 +0800

    1

--- courses/mathematics/urls.py
+++ courses/mathematics/urls.py
@@ -6,8 +6,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^questions$' , views.index, name="zc"),
+    url(r'^neurons$' , views.indexz, name="zcq"),
     url(r'^$' , views.index, name="zc"),
     url(r'^preview$',views.preview, name='preview'),
-    url(r'^(?P<code>.+)$', views.get, name='index')
+    url(r'^questions/(?P<code>.+)$', views.get, name='index'),
+    url(r'^previewneurons$', views.getneuron, name='index')
 
 ]
