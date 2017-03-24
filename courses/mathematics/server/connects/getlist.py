40840eae6c9ac9f876cae77612ea2bb4303b29ed
cobbb1 <531138675@qq.com>
2017-03-24 20:18:59 +0800

    Revert "Revert "1""
    This reverts commit 7da975b8efe83a992f65819f5a381bb7bab50c77.

---
+++ courses/mathematics/server/connects/getlist.py
@@ -0,0 +1,13 @@
+__author__ = 'Administrator'
+
+
+from django.core import serializers
+from django.http import HttpResponse
+from mathematics.models import Connect,Neuron
+
+from django.db.models import F
+
+
+def getlist(request,chapter_id):
+    connects = Connect.objects.filter(begin__chapter=chapter_id,ending__chapter=chapter_id)
+    return HttpResponse(serializers.serialize("json",connects),content_type="applcation/text")
