906f943cbc868f60ed266076809d1d1591bcca27
cobbb1 <531138675@qq.com>
2017-04-05 17:20:11 +0800

    1

--- courses/mathematics/server/use/url.py
+++ courses/mathematics/server/use/url.py
@@ -7,6 +7,7 @@ from get import get
 from getNeuron import getNeuron
 
 urlpatterns = [
+    url(r'^(?P<user_id>.+?)/neurons/(?P<chapter_id>)' , getNeuron),
     url(r'^(?P<token>.+?)$' , get),
-    url(r'^(?P<user_id>.+?)/neurons/(?P<chapter_id>.+?)' , getNeuron)
+
 ]
