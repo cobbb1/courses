2c7eb9b8f9b5ce0c39afc5f8af3f481084b15cde
cobbb1 <531138675@qq.com>
2017-04-06 22:27:37 +0800

    1

--- courses/mathematics/server/question/getlist.py
+++ courses/mathematics/server/question/getlist.py
@@ -11,8 +11,9 @@ from mathematics.server.getresponse import get_response
 
 def getlist(request):
     w = Question.objects.only("id","category","code","difficulty","sensitivity")
+    print(w)
     if request.GET.has_key("neurons"):
-        w = Neuron.objects.filter(id=int(request.GET["neurons"]))[0].question_set.only("code","category","id")
+        w = Neuron.objects.filter(id=int(request.GET["neurons"]))[0].question_set.only("question__code","question__category","question__id")
     if request.GET.has_key("difficulty"):
         w = w.filter(difficulty=int(request.GET["difficulty"]))
     return get_response(200,serialize("json",w),{})
