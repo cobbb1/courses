f92e847f98fca6c36ef1e0e25498600d303ece12
cobbb1 <531138675@qq.com>
2017-04-06 21:39:55 +0800

    1

--- courses/mathematics/server/use/neuron.py
+++ courses/mathematics/server/use/neuron.py
@@ -11,14 +11,11 @@ def neuron(userid,questionid):
     e = questionid.linkneuron.all()
     for z in e:
         m = UserNeuron.objects.filter(neuronid=z.id,userid=userid.id)
-        n = UserQuestion.objects.filter(questionid__linkneuron__id=z.id,userid=userid.id).values("questionid").annotate(counts=Count(questionid,distinct=True))
+        n = z.linkneuron_set.all()
         print(n)
         w = UserQuestion.objects.filter(questionid__linkneuron__id=z.id,userid=userid.id,correct="right").values("questionid").annotate(counts=Count(questionid,distinct=True))
         print(w)
-        if w.count()==0:
-            l = 0
-        else:
-            l = float(len(w))/len(n)
+        l = float(len(w)/n.count())
         if m.count()==0:
             j = UserNeuron(neuronid=z,userid=userid,familiar=l)
             j.save()
