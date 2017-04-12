4e8f16460bfac526b12cadf7bbb0cf44a104d1bb
cobbb1 <531138675@qq.com>
2017-04-12 19:19:52 +0800

    1

--- courses/mathematics/server/use/get.py
+++ courses/mathematics/server/use/get.py
@@ -2,15 +2,17 @@ __author__ = 'Administrator'
 
 
 
-from django.core import serializers
+
+from django.http import HttpResponseRedirect
 from mathematics.server.getresponse import get_response
 from mathematics.models import Users
 
+
 def get(request,token):
-    users = Users.objects.filter(token=token)
-    if users.count()==0:
-        return get_response(403,"{'message':''}",{})
+    z = Users.objects.filter(token=token)
+    if z.count()==0:
+        return get_response(200,'{"message":"you are not the people in the program"}')
     else:
-        z = get_response(200,"{'userid':'%s'}" % users[0].names,{"token":token})
-        z["location"] = "http://lala.ust.hk:8000/"
-        return z
+        o = HttpResponseRedirect("http://lala.ust.hk:8000")
+        o.set_cookie("token",token)
+        return o
