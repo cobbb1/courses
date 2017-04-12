da92e66710e4c204c201fd5046606ed580b957ac
cobbb1 <531138675@qq.com>
2017-04-12 18:57:31 +0800

    1

--- courses/mathematics/server/use/get.py
+++ courses/mathematics/server/use/get.py
@@ -11,4 +11,6 @@ def get(request,token):
     if users.count()==0:
         return get_response(403,"{'message':''}",{})
     else:
-        return get_response(200,"{'userid':'%s'}" % users[0].names,{"token":token})
+        z = get_response(200,"{'userid':'%s'}" % users[0].names,{"token":token})
+        z["location"] = "/"
+        return z
