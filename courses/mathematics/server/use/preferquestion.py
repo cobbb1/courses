3f5ea0370174084e327f5b7539100ee49913cbd1
cloudray8580 <240078316@qq.com>
2017-04-25 14:11:21 +0800

    fix bugs

--- courses/mathematics/server/use/preferquestion.py
+++ courses/mathematics/server/use/preferquestion.py
@@ -14,16 +14,18 @@ import re
 @csrf_exempt
 def preferquestions(request,questionid,userid):
     if request.GET.has_key("choice")==0:
-        return get_response(403,"",{})
+        return get_response(403,"no choice",{})
     e = Users.objects.filter(id=userid)
     if e.count()==0:
-        return get_response(403,"",{})
+        return get_response(403,"no such user",{})
     z = Question.objects.filter(id=questionid)
     if z.count()==0:
-        return get_response(403,"",{})
+        return get_response(403,"no such question",{})
     o = Prefer.objects.filter(questionid=questionid,userid=userid)
-    if o.count()!=0:
-        return get_response(200,"",{})
-    o = Prefer(questionid=z[0],userid=e[0],answer=int(request.GET["choice"]),time=datetime.datetime.now(),right=1)
-    o.save()
-    return get_response(200,"",{})
+    if o.count() == 0:
+        # return get_response(200,"",{})
+        o = Prefer(questionid=z[0],userid=e[0],answer=int(request.GET["choice"]),time=datetime.datetime.now(),right=1)
+        o.save()
+        return get_response(200,"success",{})
+    else:
+        return get_response(500, "failed", {})
