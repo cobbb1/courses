0c3a7248c89be935278e7f5fc117a5865b778abc
cobbb1 <531138675@qq.com>
2017-06-03 01:22:40 +0800

    1

--- courses/mathematics/server/use/getquestion.py
+++ courses/mathematics/server/use/getquestion.py
@@ -11,4 +11,6 @@ from mathematics.models import UserQuestion
 
 def getquestion(request,userid):
     e = UserQuestion.objects.filter(righte=1,userid=userid)
+    if request.GET.has_key("right"):
+        e = UserQuestion.objects.filter(righte=1,userid=userid,correct=request.GET["right"])
     return get_response(200,serialize("json",e),{})
